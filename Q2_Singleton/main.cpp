8b826910a14f9b71111c8f13378d97e976726b73
钟华松 <zhonghsuestc@163.com>
2017-05-11 16:40:19 +0800

    update

--- Q2_Singleton/main.cpp
+++ Q2_Singleton/main.cpp
@@ -7,14 +7,21 @@ private:
     Singleton1() {
 
     };
-    static Singleton1* instance = new Singleton1();
+    static Singleton1* m_pInstance;
 public:
-    static Singleton1* GetInstance() {
-        return instance;
-    };
+    static Singleton1* GetInstance();
 };
+Singleton1* Singleton1::m_pInstance = NULL;
+Singleton1* Singleton1::GetInstance() {
+    if (m_pInstance == NULL) {
+        m_pInstance = new Singleton1();
+    }
+    return m_pInstance;
+}
 
 int main() {
     Singleton1* single = Singleton1::GetInstance();
+    Singleton1* single2 = single->GetInstance();
+    Singleton1& ref = * Singleton1::GetInstance();
     return 0;
 }
